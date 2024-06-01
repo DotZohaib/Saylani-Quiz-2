1b3c705d9f74d1f8cef03d7a52d9c3a293ce8504
ZOHAIB ALI DAYO <154052553+DotZohaib@users.noreply.github.com>
2024-06-01 23:36:00 +0500

    Add files via upload
    Saylani-Quiz-2

---
+++ file.js
@@ -0,0 +1,26 @@
+// let bill = {
+//     customerName: "junaid",
+//     timestamp: 234
+// };
+// console.log(bill.customerName, bill.timestamp);
+
+// loop
+
+// for (let a = 0; a < 10; a++) {
+//     console.log(a)  
+// }
+
+
+// let value = 1
+
+// while (value <= 10) {
+//     console.log(value);
+//     console.log("maria");
+//     value++
+
+// }
+
+
+let database[] = [
+                  
+]
